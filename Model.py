cffa0d49a29876b74a55c606f24e3089ead2dd65
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2023-11-06 21:15:11 -0500

    Implemented self-play function

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import game
 class TicTacZero(torch.nn.Module):
     def __init__(self):
         super(TicTacZero, self).__init__()
@@ -22,8 +23,8 @@ class TicTacZero(torch.nn.Module):
         x = self.input(x)
         x = self.dense1(self.inner_activation(x))
         x = self.dense2(self.inner_activation(x))
-        x = self.output(x)
-
+        x = self.output(self.inner_activation(x))
+        x = torch.relu(x)
         return x
 
     def choose_move(self, boardstate):
@@ -32,13 +33,40 @@ class TicTacZero(torch.nn.Module):
         legal_moves = (x + o - 1) * -1
         legal_moves = torch.FloatTensor(legal_moves)
         move_values = self.forward(boardstate)
-        legal_move_values = move_values * legal_moves
-        move_choice = [0+int(i == max(legal_move_values)) for i in legal_move_values]
+        legal_move_values = (move_values * legal_moves) + legal_moves
+        print(legal_move_values)
+        move_choice = [0+int(i == torch.argmax(legal_move_values)) for i in range(9)]
         return move_choice
 
     def play_game(self):
         boardstate = [0,0,0,0,0,0,0,0,0,
                       0,0,0,0,0,0,0,0,0]
+
+        game_status = 0
+
+        print('starting loop')
+        while game_status == 0:
+            x = boardstate[0:9]
+            o = boardstate[9:]
+            if sum(o) == sum(x):
+                move = self.choose_move(boardstate)
+                x = [sum(i) for i in zip(x, move)]
+                token = 'x'
+            else:
+                move = self.choose_move(boardstate)
+                o = [sum(i) for i in zip(o, move)]
+                token = 'o'
+            print(token)
+            print(move)
+            boardstate = x+o
+            print(boardstate)
+            game_status = game.evaluate_game(boardstate, token)
+            print(game.render_gamestate(boardstate))
+            print(game_status)
+        return game_status, boardstate
+
+
+
 
 
 
