6c10b4d86c18377cc1ba11f2f281ca7d0a28d95e
cwstoughton <67300513+cwstoughton@users.noreply.github.com>
2023-11-11 20:25:01 -0500

    implemented training func. Doesn't work well yet.

--- Model.py
+++ Model.py
@@ -23,8 +23,9 @@ class TicTacZero(torch.nn.Module):
         x = self.input(x)
         x = self.dense1(self.inner_activation(x))
         x = self.dense2(self.inner_activation(x))
+        x = self.dense3(self.inner_activation(x))
         x = self.output(self.inner_activation(x))
-        x = torch.relu(x)
+        x = torch.softmax(x,0)
         return x
 
     def choose_move(self, boardstate):
@@ -34,7 +35,6 @@ class TicTacZero(torch.nn.Module):
         legal_moves = torch.FloatTensor(legal_moves)
         move_values = self.forward(boardstate)
         legal_move_values = (move_values * legal_moves) + legal_moves
-        print(legal_move_values)
         move_choice = [0+int(i == torch.argmax(legal_move_values)) for i in range(9)]
         return move_choice
 
@@ -43,8 +43,8 @@ class TicTacZero(torch.nn.Module):
                       0,0,0,0,0,0,0,0,0]
 
         game_status = 0
-
-        print('starting loop')
+        memory = []
+        #print('starting loop')
         while game_status == 0:
             x = boardstate[0:9]
             o = boardstate[9:]
@@ -56,14 +56,25 @@ class TicTacZero(torch.nn.Module):
                 move = self.choose_move(boardstate)
                 o = [sum(i) for i in zip(o, move)]
                 token = 'o'
-            print(token)
-            print(move)
+
             boardstate = x+o
-            print(boardstate)
+            memory.append(boardstate)
             game_status = game.evaluate_game(boardstate, token)
-            print(game.render_gamestate(boardstate))
-            print(game_status)
-        return game_status, boardstate
+
+        return memory, game_status
+
+    def learn(self):
+        x,y = self.play_game()
+        y = [y for i in range(9)]
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.4)
+        loss_fn = torch.nn.L1Loss()
+        for i in x:
+            optimizer.zero_grad()
+            y_hat = self.choose_move(i)
+            loss = loss_fn(torch.FloatTensor(y_hat),torch.FloatTensor(y))
+            loss.requires_grad = True
+            loss.backward()
+            optimizer.step()
 
 
 
